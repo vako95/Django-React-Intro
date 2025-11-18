31233e390afa5178f712dfcba170c5342480e683
User <vagifhuseynov95@gmail.com>
2025-11-19 00:56:30 +0400

    finish Maintaince

--- src/pages/status/Maintenance/Maintenance.jsx
+++ src/pages/status/Maintenance/Maintenance.jsx
@@ -1,22 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
+import { Link } from "react-router-dom";
 import { useTimer } from 'react-timer-hook';
-import BackdropContainer from '../../../components/ui/BackdropContainer/BackdropContainer';
-import { Container } from "@components/ui"
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay } from "swiper/modules";
-import { maintainceItem } from "../../../constants/sections"
-import { Link } from "react-router-dom"
-import maintenceLogo from "./assets/img/logo-wide-white.png";
-import "./Maintenance.css";
 import { socialLinks } from '../../../constants/socials';
+import { motion, AnimatePresence } from "framer-motion";
+import "./Maintenance.css";
+import maintenceLogo from "./assets/img/logo-wide-white.png";
 import { ICON_MAP } from "@constants/map.js";
-
-
+import { Container } from "@components/ui";
 const images = [
-    { id: 1, src: "https://media.cntraveler.com/photos/5a68c6949e34830eca77d87b/16:9/w_1920,c_limit/Beekman_2018_Beekman_BarRoom.jpg" },
-    { id: 2, src: "https://encrypted-tbn1.gstatic.com/licensed-image?q=tbn:ANd9GcTu-GSY_bXjggu92Go8I0Od4bEoIE-RnSuaCRmN5xcL4lfSDQI169Wyg5hK0VegSLUJjpqlG47veDZ33C0" },
+    { id: 1, src: "https://wallpapers.com/images/high/restaurant-in-grand-resort-lagonissi-greece-znonmfdj26fii6ol.webp" },
+    { id: 2, src: "https://images.unsplash.com/photo-1611892440504-42a792e24d32?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
     { id: 3, src: "https://media.cntraveler.com/photos/5a68c6949e34830eca77d87b/16:9/w_1920,c_limit/Beekman_2018_Beekman_BarRoom.jpg" },
-
 ];
 const Maintenance = ({ expiryTimestamp }) => {
     const defaultTime = React.useMemo(() => {
@@ -29,7 +23,6 @@ const Maintenance = ({ expiryTimestamp }) => {
 
     const {
         totalSeconds,
-        milliseconds,
         seconds,
         minutes,
         hours,
@@ -38,23 +31,23 @@ const Maintenance = ({ expiryTimestamp }) => {
     } = useTimer({
         expiryTimestamp: finalTimestamp,
         onExpire: () => console.warn("Timer expired"),
-        // interval: 20,
     });
 
     const [imgIdx, setImgIdx] = useState(0);
 
 
+
     useEffect(() => {
         let lastTime = performance.now();
-        const interval = 1000
+        const interval = 5000
         let frameId = null
         const step = (timeStamp) => {
             const delta = timeStamp - lastTime
             if (delta >= interval) {
                 setImgIdx((prev) => prev >= images.length - 1 ? 0 : prev + 1)
+
                 lastTime = timeStamp;
             }
-
             frameId = requestAnimationFrame(step);
         }
         frameId = requestAnimationFrame(step)
@@ -64,6 +57,30 @@ const Maintenance = ({ expiryTimestamp }) => {
     return (
         <>
             <div className="maintenance">
+                <div className="maintenance__slider">
+                    <div className="maintenance__slider-backdrop"
+                        style={{
+                            backgroundImage: `url(${images[imgIdx].src})`
+                        }}
+                    >
+                        <AnimatePresence mode="wait">
+                            <motion.img
+                                key={imgIdx}
+                                src={images[imgIdx].src}
+                                className="maintenance__slider-backdrop-img"
+                                initial={{ opacity: 0, scale: 1.6 }}
+                                animate={{ opacity: 1, scale: 1.4 }}
+                                exit={{ opacity: 1, scale: 1 }}
+                                transition={{
+                                    opacity: { duration: 1, ease: 'easeInOut' },
+                                    scale: { duration: 6, ease: 'easeInOut' },
+                                }}
+                            />
+                        </AnimatePresence>
+
+
+                    </div>
+                </div>
 
                 <Container>
                     <div className="maintenance__content">
@@ -80,7 +97,6 @@ const Maintenance = ({ expiryTimestamp }) => {
                                         Our website is under construction.
                                         We are working on something really amazing!
                                     </span>
-                                    {/* <p>{isRunning ? 'Running' : 'Not running'}</p> */}
                                 </div>
                                 <div className="maintenance__about-social">
                                     <div className="maintenance__about-social-heading">
@@ -106,7 +122,6 @@ const Maintenance = ({ expiryTimestamp }) => {
                                 </div>
                             </div>
 
-
                         </div>
                         <div className="maintenance__content maintenance__content--right">
                             <div className="maintenance__timer">
@@ -120,7 +135,6 @@ const Maintenance = ({ expiryTimestamp }) => {
                                         <span className="maintenance__timer-unit-value">
                                             {days}
                                         </span>
-
                                         <h2 className="maintenance__timer-unit-title">
                                             Days
                                         </h2>
@@ -147,58 +161,16 @@ const Maintenance = ({ expiryTimestamp }) => {
                                         <span className="maintenance__timer-unit-value">
                                             {seconds}
                                         </span>
-
                                         <h2 className="maintenance__timer-unit-title">
                                             Seconds
                                         </h2>
                                     </div>
-                                    <div className="maintenance__timer-unit">
-                                        <span className="maintenance__timer-unit-value">
-                                            {milliseconds}
-                                        </span>
 
-                                        <h2 className="maintenance__timer-unit-title">
-                                            Milliseconds
-                                        </h2>
-                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </Container>
-
-                <div className="maintenance__container">
-                    <div className="maintenance__slider">
-                        {/* {images.map((item) => (
-                            <div className="maintenance__slider-backdrop">
-                                <img className="maintenance__slider-backdrop-img" key={item.id} src={item.src} alt="" />
-                            </div>
-                        ))} */}
-
-                        <div className="maintenance__slider-backdrop">
-                            <img
-                                className="maintenance__slider-backdrop-img"
-                                src={images[imgIdx]?.src}
-                                alt="" />
-                        </div>
-                    </div>
-
-                    {/* <Swiper className="maintenance__slider"
-                        modules={[Autoplay]}
-                        autoplay={{
-                            delay: 4000,
-                            disableOnInteraction: false,
-                        }}
-                    >
-                        {maintainceItem.map((item) => (
-                            <SwiperSlide className="maintenance__slider-panel" key={item.id}>
-                                <div className="maintance__backdrop">
-                                    <img className="maintance__backdrop-img" src={item.img} alt="" />
-                                </div>
-                            </SwiperSlide>
-                        ))}
-                    </Swiper> */}
-                </div>
             </div>
         </>
     );
